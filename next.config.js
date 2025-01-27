e2bf0e49755b20f669738a53c2df53db87e4e0e0
shamoon <4887959+shamoon@users.noreply.github.com>
2025-01-27 07:36:14 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const { i18n } = require("./next-i18next.config");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
   },
   reactStrictMode: true,
   output: "standalone",
